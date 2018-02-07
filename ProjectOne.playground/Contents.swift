53da16df183300a1d13c88a40973708c7cbb61f6
Melody <melodyio@hawaii.edu>
2018-02-07 03:00:57 -1000

    Finished Project One

---
+++ ProjectOne.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Variable Coding Challenge #1
+
+var firstString = "Hello there "
+let secondString = "I am hungry"
+var thirdString = firstString + secondString
+print(thirdString)
+
+//Variable Coding Challenge #2
+
+let celsius: Float
+let fahrenheit: Float = 50
+celsius = (fahrenheit-32)/1.8
+let statement = "Today is \(celsius) degrees celsius"
+print(statement)
+
+//Boolean Variables Coding Challenge #1
+
+var numberOne: Int = 1
+var numberTwo: Int = 1
+var numberThree = numberOne == numberTwo
+
+//If statement Coding Challenge #1
+
+let iceCream = "I want to eat strawberry ice cream"
+let cake = "I want to eat strawberry cake"
+
+if iceCream>cake {
+    print("but I'll get fat")
+}
+else {
+    print("but I dont have the ingredients")
+}
+
+
